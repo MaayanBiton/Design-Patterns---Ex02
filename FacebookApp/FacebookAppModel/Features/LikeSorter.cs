f592795931dfc77d44bd4f61716e120ae146031f
MaayanBiton <61507474+MaayanBiton@users.noreply.github.com>
2020-03-05 13:39:21 +0200

    Add files via upload

---
+++ FacebookApp/FacebookAppModel/Features/LikeSorter.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FacebookWrapper.ObjectModel;
+using FacebookWrapper;
+
+namespace FacebookApp
+{
+    public class LikeSorter : IFeature
+    {
+        private const int                   k_StartAmountOfLikes = 1;
+
+        public LikeSorter(UserProxy i_LoggedInUser)
+        {
+            this.FriendsLikeCounter = new Dictionary<User, int>();
+            this.LoggedInUser = i_LoggedInUser;
+        }
+
+        public Dictionary<User, int> FriendsLikeCounter { get; set; }
+
+        public UserProxy LoggedInUser { get; set; }
+
+        public void FeatureCliked()
+        {
+           this.RunLikeSorter();
+        }
+
+        public void RunLikeSorter()
+        {
+            try
+            {
+                for (int i = 0; i < this.LoggedInUser.LoggedInUser.Posts.Count; i++)
+                {
+                    foreach (User user in this.LoggedInUser.LoggedInUser.Posts[i].LikedBy)
+                    {
+                        if (this.isFriendInDictionary(user.Name))
+                        {
+                            this.updateRecordInDictionary(user);
+                        }
+                        else
+                        {
+                            this.FriendsLikeCounter.Add(user, k_StartAmountOfLikes);
+                        }
+                    }
+                }
+
+                this.sortedDictionary();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private bool isFriendInDictionary(string i_NameOfFriend)
+        {
+            bool isExist = false;
+
+                foreach (User user in this.FriendsLikeCounter.Keys)
+                {
+                    if (user.Name.Equals(i_NameOfFriend))
+                    {
+                        isExist = true;
+                        break;
+                    }
+                }
+            
+            return isExist;
+        }
+
+        private void updateRecordInDictionary(User i_UserToUpdate)
+    {
+        foreach (User userInDictionary in this.FriendsLikeCounter.Keys)
+        {
+            if (userInDictionary.Name.Equals(i_UserToUpdate.Name))
+            {
+                int currentUserAmountOfLikesInDictionary = this.FriendsLikeCounter[userInDictionary];
+                currentUserAmountOfLikesInDictionary++;
+                this.FriendsLikeCounter[userInDictionary] = currentUserAmountOfLikesInDictionary;
+                break;
+            }
+        }
+    }
+
+        private void sortedDictionary()
+        {
+            try
+            {
+                this.FriendsLikeCounter.OrderByDescending(likeAmount => likeAmount.Value);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ FacebookApp/FacebookAppViewModel/FacebookAppFacade.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FacebookWrapper;
+using FacebookWrapper.ObjectModel;
+
+namespace FacebookApp
+{
+    public class FacebookAppFacade
+    {
+        private static readonly string[] sr_AgeRange = new string[] { "18-21", "22-25", "26-30", "31-40", "41-50", "51-65", "66+" };
+
+        public AppSettings AppSettings { get; set; }
+
+        internal LoginResult        m_LoginResult;
+        internal UserProxy          m_LoggedInUserProxy;
+
+        public string[] AgeRange
+        {
+            get
+            {
+                return sr_AgeRange;
+            }
+        }
+
+        public void LoginAndInit()
+        {
+            AppSettings.LoginAndInit(out m_LoginResult, out this.m_LoggedInUserProxy);
+        }
+
+        public void PostStatus(string i_NewPost)
+        {
+            IFeature post;
+            if (!string.IsNullOrEmpty(i_NewPost))
+            {
+                post = FeaturesFactoryMethod.Create(MethodFactoryTypes.eFacebookFeatureType.Post, this.m_LoggedInUserProxy);
+                (post as PostNewStatus).NewPost = i_NewPost;
+                post.FeatureCliked();
+            }
+        }
+
+        public Dictionary<User, int> SortLikes()
+        {
+            IFeature sort;
+            sort = FeaturesFactoryMethod.Create(MethodFactoryTypes.eFacebookFeatureType.LikeSorter, this.m_LoggedInUserProxy);
+            sort.FeatureCliked();
+            return (sort as LikeSorter).FriendsLikeCounter;
+        }
+
+        public List<User> FindMatch(bool i_IsFemaleChecked, bool i_IsMaleChecked, string i_AgeRangeSelected)
+        {
+            IFeature findMatch = FeaturesFactoryMethod.Create(MethodFactoryTypes.eFacebookFeatureType.MatchFinder, this.m_LoggedInUserProxy);
+            (findMatch as MatchFinder).MatchInitializer(findMatch as MatchFinder, i_IsFemaleChecked, i_IsMaleChecked, i_AgeRangeSelected);
+
+            if ((i_IsFemaleChecked || i_IsMaleChecked) && !i_AgeRangeSelected.Equals("Select your desired age range"))
+            {
+                (findMatch as MatchFinder).FeatureCliked();
+            }
+            else
+            {
+                throw new Exception("Not all parameters are selected");
+            }
+
+            return (findMatch as MatchFinder).MatchFriends;
+        }
+
+        public List<PostAdapter> GetPosts()
+        {
+            return PostAdapter.CreateAdapterPosts(this.m_LoggedInUserProxy.LoggedInUser.Posts);
+        }
+    }
+}
